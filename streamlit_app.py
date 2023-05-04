8404f3e36894986a551611f7244846b5ceba577d
wwaa321 <30785164+wwaa321@users.noreply.github.com>
2023-05-04 19:26:58 +0800

    vv
    xccv

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,11 @@ st.sidebar.subheader("📈综合可视化看板")
 st.title("📈综合可视化看板")
 st.write("gggaaafff")
 st.markdown("##### aaa #####  ")
+
+st.markdown("***各级别合格率***  ")
+  #用折线图表现各级别合格率情况
+hegelv_tj=pd.read_excel('file/tongji.xlsx',sheet_name='Sheet3',index_col=[0],na_values=['NA'])
+st.line_chart(hegelv_tj)
+
+ st.divider()
+st.area_chart(hegelv_tj)
